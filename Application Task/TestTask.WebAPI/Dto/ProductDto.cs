4636c09e300a03d37dcd258feae4f174b0851a1a
byteslav <cesnokovaroslav85@gmail.com>
2024-02-25 11:28:43 +0300

    added client side

--- Application Task/TestTask.WebAPI/Dto/ProductDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TestTask.WebApi.Dto
-{
-    public class ProductDto
-    {
-        public int Code { get; set; }
-        public string Value { get; set; }
-    }
-}
---
+++ Backend (Task 1)/TestTask.WebAPI/Dto/ProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TestTask.WebApi.Dto
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public int Code { get; set; }
+        public string Value { get; set; }
+    }
+}
